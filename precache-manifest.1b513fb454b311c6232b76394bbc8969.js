00627658efdcdbe5d5a62f2b71c51521e9afb159
Salim <sbencosme@encorerepair.com>
2019-04-30 15:46:51 -0400

    Updates

---
+++ precache-manifest.1b513fb454b311c6232b76394bbc8969.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "16ac220af7d3db74a7c1",
+    "url": "/static/css/main.a13bc722.chunk.css"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "edc48e4696e93ab46aa7",
+    "url": "/static/js/1.edc48e46.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "16ac220af7d3db74a7c1",
+    "url": "/static/js/main.16ac220a.chunk.js"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "edc48e4696e93ab46aa7",
+    "url": "/static/css/1.fcc24200.chunk.css"
+  },
+  {
+    "revision": "5591a2020d59a8d659aa4c3b38b4d853",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3664bd2de93470a3ca8b6c4188a16e09.js"
+  "/precache-manifest.1b513fb454b311c6232b76394bbc8969.js"
 );
 
 workbox.clientsClaim();
