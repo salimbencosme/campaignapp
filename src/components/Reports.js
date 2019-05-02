4e9a6e1bc02033b2a171ae97ca2d49bf614ba830
Salim <sbencosme@encorerepair.com>
2019-05-02 07:52:38 -0400

    Done changes

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -74,6 +74,7 @@ class Reports extends Component {
                 }
             }
 
+
             currentComponent.setState({
                 votes: votesContainer
             });
@@ -84,6 +85,41 @@ class Reports extends Component {
     }
 
 
+    orderTable(votesContainer) {
+        console.log("ORDER TABLE");
+        /*ORDER */
+        var infoTemp = votesContainer;
+        var titleTemp = [];
+        var tableTempOrder = [];
+        for (var x = 0; x < infoTemp.length; x++) {
+
+            var toAdd = 0;
+            for (var f = 0; f < titleTemp.length; f++) {
+                if (infoTemp[x].electoraltable.electoralcollege.name === titleTemp[f]) {
+                    toAdd++;
+                }
+            }
+
+            if (toAdd === 0) {
+
+                titleTemp.push(infoTemp[x].electoraltable.electoralcollege.name);
+                for (var k = 0; k < votesContainer.length; k++) {
+                    if (votesContainer[k].electoraltable.electoralcollege.name === infoTemp[x].electoraltable.electoralcollege.name) {
+                        tableTempOrder.push(votesContainer[k]);
+                    }
+                }
+            }
+
+
+        }
+
+        /** END ORDER */
+
+        return tableTempOrder;
+
+    }
+
+
     createTable() {
         console.log("create table");
         console.log(this.state.votes);
@@ -91,26 +127,31 @@ class Reports extends Component {
 
         array.push(
             <tr>
-            <th>Nombre</th>
-            <th>Cédula</th>
-            <th>edad</th>
-            <th>Contactos</th>
-            <th>Centro de votacion</th>
-            <th>Mesa</th>
-            <th>Registrado</th>
-          </tr>
+                
+                    <th>NOMBRE</th>
+                    <th>CÉDULA</th>
+                    <th>EDAD</th>
+                    <th>CONTACTOS</th>
+                    <th>CENTRO</th>
+                    <th>COLEGIO</th>
+                    <th>REGISTRADO</th>
+            
+            </tr>
         );
 
         if (this.state.votes.length > 0) {
-            this.state.votes.map(item => {
+
+            var votesOrdered = this.orderTable(this.state.votes);
+
+            votesOrdered.map(item => {
                 array.push(
                     <tr>
                         <td>{item.citizen.name}</td>
                         <td>{item.citizen.identification}</td>
                         <td>{item.citizen.age}</td>
                         <td>
-                            <b>Email: </b>{item.citizen.email === undefined ? 'N/A' :item.citizen.email }
-                            <br/>
+                            <b>Email: </b>{item.citizen.email === undefined ? 'N/A' : item.citizen.email}
+                            <br />
                             <b>Telefono(s): </b>{item.citizen.phone}
                         </td>
                         <td>{item.electoraltable.electoralcollege.name}</td>
@@ -133,7 +174,7 @@ class Reports extends Component {
                     <div class="row justify-content-center">
 
                         <div class="col-12 col-md-12 col-lg-12">
-                        
+
                             <form>
 
                                 <div class="pull-left">
@@ -143,8 +184,10 @@ class Reports extends Component {
                                 <div class="card-contact border-primary rounded-0">
 
                                     <table id="customers">
-                                       {this.createTable()}
+                                            {this.createTable()}
                                     </table>
+
+
 
                                 </div>
 
